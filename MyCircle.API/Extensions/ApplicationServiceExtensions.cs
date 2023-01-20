08b5ea62a272c406bd9efd3098ceafb4bf7eba52
Junaid Malik <junmalik109@gmail.com>
2023-01-20 18:27:27 +0000

    Custom extensions added

---
+++ MyCircle.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using MyCircle.API.Contracts;
+using MyCircle.API.Data.Repository;
+using MyCircle.API.Options;
+using MyCircle.API.Profiles;
+using MyCircle.API.Services;
+
+namespace MyCircle.API.Extensions
+{
+	public static class ApplicationServiceExtensions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+		{
+			services.AddScoped<ITokenService, TokenService>();
+			services.AddScoped<IUserRepository, UserRepository>();
+			services.AddScoped<IPhotoService, PhotoService>();
+			services.AddScoped<LogUserActivity>();
+			services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+			services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+			return services;
+		}
+	}
+}
---
+++ MyCircle.API/Extensions/IdentityServiceExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using MyCircle.API.Data;
+using MyCircle.API.Entities;
+using System.Text;
+
+namespace MyCircle.API.Extensions
+{
+	public static class IdentityServiceExtension
+	{
+		public static IServiceCollection AddIdentityServices(this IServiceCollection services, IServiceCollection services, IConfiguration config)
+		{
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+						ValidateIssuer = false,
+						ValidateAudience = false
+					};
+				});
+
+			return services;
+		}
+	}
+}
